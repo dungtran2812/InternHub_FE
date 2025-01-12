585aab610af96542a3a8f0184756798a3fa14803
dungtran2812 <dunghoclaptrinh2812@gmail.com>
2025-01-12 16:02:01 +0700

    implement login api

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -6,12 +6,14 @@ import CompanyHome from "@/containers/CompanyDashboard/CompanyHome/CompanyHome";
 import AboutUsPage from "@/containers/AboutUs/AboutUs";
 import BlogPage from "@/containers/BlogPage/BlogPage";
 import Login from "@/containers/Authentication/Login";
+import SignUp from "@/containers/Authentication/SignUp";
 
 //luồng ruoting chính sẽ là component này
 const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/login" element={<Login />}/>
+      <Route path="/signup" element={<SignUp />}/>
       <Route path="/" element={<HomeLayout />}>
         <Route path="/" element={<Home />}/>
         <Route path="/about" element={<AboutUsPage />}/>
