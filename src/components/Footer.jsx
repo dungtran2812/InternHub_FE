1e75b33b6fc9f3f99e801e1539769532ef7f468f
Phuong Dao Hoai <daophuong131.work@gmail.com>
2025-01-25 20:06:51 +0700

    add footer v2
    have link policy

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -109,8 +109,12 @@ const Footer = () => {
 						<p className="text-white">
 							Email: internhub.kalocs@gmail.com
 						</p>
+						<p className="text-white">
+							Location: Lot E2a-7, Road D1, Long Thanh My, Thu Duc
+							City, Ho Chi Minh City
+						</p>
 					</div>
-					<div className="flex flex-col gap-2">
+					{/* <div className="flex flex-col gap-2">
 						<h3 className="text-white font-bold text-lg">
 							Mobile App
 						</h3>
@@ -126,22 +130,42 @@ const Footer = () => {
 								className="h-[38px] w-[131px]"
 							/>
 						</div>
-					</div>
+					</div> */}
 				</div>
 
 				<Separator className="my-6 bg-white/20" />
 
 				{/* Company Info */}
 				<div className="flex flex-col gap-2">
-					<h2 className="text-white font-bold italic text-xl">
+					{/* <h2 className="text-white font-bold italic text-xl">
 						Limited Liability Company KALOCS
-					</h2>
-					<p className="text-white text-sm">
-						Location: Lot E2a-7, Road D1, Long Thanh My, Thu Duc
-						City, Ho Chi Minh City
-					</p>
+					</h2> */}
 					<p className="text-white text-sm">
 						Copyright © Limited Liability Company KALOCS
+					</p>
+					<p className="text-white text-sm">
+						Sử dụng nội dung ở trang này và dịch vụ tại&nbsp;
+						<a
+							href="https://www.internhub.com"
+							className="underline text-pink-400"
+						>
+							InternHub
+						</a>
+						&nbsp;có nghĩa là bạn đồng ý với&nbsp;
+						<a
+							href="/terms-of-service"
+							className="underline text-pink-400"
+						>
+							Thoả thuận sử dụng
+						</a>
+						&nbsp;và&nbsp;
+						<a
+							href="/privacy-policy"
+							className="underline text-pink-400"
+						>
+							Chính sách bảo mật
+						</a>
+						&nbsp;của chúng tôi
 					</p>
 				</div>
 			</div>
