71796ccee0b89085fdd6d0991248fb32fbe6cbcc
TanHuynh68 <tanhpce171112@fpt.edu.vn>
2025-01-30 18:26:27 +0700

    use line chart of amchart4

--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -1,108 +1,52 @@
-
-import Chart from "react-apexcharts";
+import React, { useEffect } from "react";
+import * as am4core from "@amcharts/amcharts4/core";
+import * as am4charts from "@amcharts/amcharts4/charts";
+import * as am4themes_animated from "@amcharts/amcharts4/themes/animated";
 
 const LineChart = () => {
-  const chartConfig = {
-    series: [
-      {
-        name: "Sales",
-        data: [50, 40, 300, 320, 500, 350, 200, 230, 500],
-      },
-    ],
-    chart: {
-      type: "line",
-      height: 240,
-      toolbar: {
-        show: false,
-      },
-    },
-    title: {
-      text: "",
-    },
-    dataLabels: {
-      enabled: false,
-    },
-    colors: ["#020617"],
-    stroke: {
-      lineCap: "round",
-      curve: "smooth",
-    },
-    markers: {
-      size: 0,
-    },
-    xaxis: {
-      axisTicks: {
-        show: false,
-      },
-      axisBorder: {
-        show: false,
-      },
-      labels: {
-        style: {
-          colors: "#616161",
-          fontSize: "12px",
-          fontFamily: "inherit",
-          fontWeight: 400,
-        },
-      },
-      categories: [
-        "Apr",
-        "May",
-        "Jun",
-        "Jul",
-        "Aug",
-        "Sep",
-        "Oct",
-        "Nov",
-        "Dec",
-      ],
-    },
-    yaxis: {
-      labels: {
-        style: {
-          colors: "#616161",
-          fontSize: "12px",
-          fontFamily: "inherit",
-          fontWeight: 400,
-        },
-      },
-    },
-    grid: {
-      show: true,
-      borderColor: "#dddddd",
-      strokeDashArray: 5,
-      xaxis: {
-        lines: {
-          show: true,
-        },
-      },
-      padding: {
-        top: 5,
-        right: 20,
-      },
-    },
-    fill: {
-      opacity: 0.8,
-    },
-    tooltip: {
-      theme: "dark",
-    },
-  };
+  useEffect(() => {
+    // Kích hoạt theme animation
+    am4core.useTheme(am4themes_animated.default);
 
-  return (
-    <div className="relative flex flex-col rounded-xl bg-white text-gray-700 shadow-md">
-      <div className="relative mx-4 mt-4 flex flex-col gap-4 md:flex-row md:items-center">
-        <div>
-          <h6 className="font-semibold text-base text-blue-gray-900">
-          Interns summary
-          </h6>
-        </div>
-      </div>
-      <div className="pt-6 px-2 pb-0">
-        <Chart options={chartConfig} series={chartConfig.series} type="line" height={240} />
-      </div>
-    </div>
-  );
+    // Tạo chart
+    let chart = am4core.create("chartdiv", am4charts.XYChart);
+    chart.paddingRight = 20;
+
+    // Tạo dữ liệu
+    let data = [];
+    let date = new Date();
+    let value = 100;
+    for (let i = 0; i < 1200; i++) {
+      value += Math.round(Math.random() * 10 - 5);
+      data.push({ date: new Date(date), value: value });
+      date.setDate(date.getDate() + 1);
+    }
+    chart.data = data;
+
+    // Tạo trục X (DateAxis)
+    let dateAxis = chart.xAxes.push(new am4charts.DateAxis());
+    dateAxis.renderer.grid.template.location = 0;
+
+    // Tạo trục Y (ValueAxis)
+    let valueAxis = chart.yAxes.push(new am4charts.ValueAxis());
+
+    // Tạo series (Line Series)
+    let series = chart.series.push(new am4charts.LineSeries());
+    series.dataFields.dateX = "date";
+    series.dataFields.valueY = "value";
+    series.tooltipText = "{valueY}";
+    series.strokeWidth = 2;
+
+    // Tạo Scrollbar
+    chart.scrollbarX = new am4core.Scrollbar();
+
+    // Cleanup chart khi component bị unmount
+    return () => {
+      chart.dispose();
+    };
+  }, []);
+
+  return <div id="chartdiv" style={{ width: "100%", height: "500px" }}></div>;
 };
 
 export default LineChart;
