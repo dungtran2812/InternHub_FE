4da3f77df1bcec06022cf3cb8384eaa914d97763
TanHuynh68 <tanhpce171112@fpt.edu.vn>
2025-02-10 16:50:06 +0700

    company detail page

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -25,7 +25,7 @@ import InternshipEvaluation from "@/containers/University/InternshipEvaluation/I
 import JobPostDetail from "@/containers/University/JobPostDetail/JobPostDetail";
 import UniversityInternFeedback from "@/containers/University/InternFeedback/InternFeedback";
 import ManageIntern from "@/containers/University/ManageIntern/ManageIntern";
-
+import CompanyDetail from "@/containers/CompanyDetail/CompanyDetail";
 //luồng ruoting chính sẽ là component này
 const AppRoutes = () => {
   return (
@@ -40,7 +40,8 @@ const AppRoutes = () => {
         <Route path="/blog" element={<BlogPage />} />
         <Route path="/companies" element={<CompanyList />} />
         <Route path="/employee-profile" element={<EmployeeProfile />} />
-        <Route path="/company-profile" element={<CompanyProfile />} />
+        <Route path="/job-detail" element={<CompanyProfile />} />
+        <Route path="/company-detail" element={<CompanyDetail />} />
         <Route />
         <Route />
       </Route >
@@ -55,7 +56,10 @@ const AppRoutes = () => {
         <Route path="/recruiter" element={<CompanyHome />} />
         <Route path="/recruiter/manage-applications" element={<ManageApplication />} />
         <Route path="/recruiter/manage-intern-feedbacks" element={<ManageInternFeedback />} />
+       
       </Route>
+
+   
 
       <Route path="/university" element={<UniversityLayout />}>
         <Route path="/university" element={<UniversityHomePage />} />
